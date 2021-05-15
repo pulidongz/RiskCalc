7d9f4ad9959e86ec26ec8febaa9cbd471a7ceacf
pulidongz <paogravoso@gmail.com>
2021-05-16 03:49:03 +0800

    Working CDOS

--- src/components/formulas/cdos.js
+++ src/components/formulas/cdos.js
@@ -1,69 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-import {
-    Grid,
-    Paper,
-    Typography,
-} from "@material-ui/core";
+import React from "react";
 
-import { makeStyles } from "@material-ui/core/styles";
+export default function cdos(props) {
+  const {
+		val1,
+		val2, 
+		val3, 
+		val4, 
+		val5,
+		val6, 
+		val7,
+		val8, } = props;
 
-
-const useStyles = makeStyles({
-	root:{
-	maxWidth: 600,
-			paddingTop: 10,
-			display: 'flex',
-			margin: 'auto',
-			flexWrap: 'wrap',
-	},
-	card: {
-		display: 'flex',
-		margin: 'auto',
-		flexWrap: 'wrap',
-		padding: 10,
-	},
-	questionnaire:{
-		paddingLeft: 10,
-		paddingRight: 40
-	},
-	slider: {
-			width: 200
-	},
-	media: {
-		height: 200,
-	},
-});
-
-export default function ResultPage(props){
-    const classes = useStyles();
-    const {
-		user, criteria,
-		val1, setVal1,
-		val2, setVal2,
-		val3, setVal3,
-		val4, setVal4,
-		val5, setVal5,
-		val6, setVal6,
-		val7, setVal7,
-		val8, setVal8, } = props;
-		const [constdelayos, setConstdelayos] = useState(null);
-		
-		// Construction Delay, Owner Side
-		// <cdos val1={val1} val2={val2} val3={val3} val4={val4} val5={val5} val6={val6} val7={val7} val8={val8} />;
-		//setConstdelayos(cdos);
-		// Construction Delay, Contractor Side
-		// const cdcs = <cdos val1={val1} val2={val2} val3={val3} val4={val4} val5={val5} val6={val6} val7={val7} val8={val8} />;
-		// Workmanship Defects, Owner Side
-		// const wdos = <cdos val1={val1} val2={val2} val3={val3} val4={val4} val5={val5} val6={val6} val7={val7} val8={val8} />;
-		// Workmanship Defects, Contractor Side
-		// const wdcs = <cdos val1={val1} val2={val2} val3={val3} val4={val4} val5={val5} val6={val6} val7={val7} val8={val8} />;
-		
-		useEffect(() => {
-			setConstdelayos(computeCDOS());
-		}, []); 
-
-		const computeCDOS = () => {
-		//------------------------------------------START OF CALCULATIONS------------------------------------------//
+//------------------------------------------START OF CALCULATIONS------------------------------------------//
 		
 		// CONVERT STRING TO INTEGER
 		var tval1 = parseInt(val1);
@@ -89,37 +37,23 @@ export default function ResultPage(props){
 
 		// GREEK VALUES - CONSTRUCTION DELAY
 		var cd_a = 0.58551*OPPDn + 0.56023*ODMDn - 0.64149*ODPDn - 0.040093*CICn + 0.10457*IACSn + 0.031359*PCIDn + 0.45703*PSn - 0.17032*NPn - 1.8498;
-
 		var cd_b = 0.15769*OPPDn + 0.43467*ODMDn + 0.34215*ODPDn + 0.53135*CICn + 0.053462*IACSn + 0.32618*PCIDn + 0.349*PSn - 0.079944*NPn - 0.75369;
-
 		var cd_g = -Math.abs(0.45043)*OPPDn - 0.76694*ODMDn + 0.61899*ODPDn + 0.13261*CICn - 0.22672*IACSn + 0.15984*PCIDn - 0.54776*PSn - 0.21265*NPn + 1.3961;
-
 		var cd_d = 0.15964*OPPDn + 0.3555*ODMDn + 0.33444*ODPDn + 0.46211*CICn + 0.0059993*IACSn + 0.53318*PCIDn + 0.1585*PSn - 0.078339*NPn - 0.91647;
-
 		var cd_e = -Math.abs(1.3076)*OPPDn + 1.2664*ODMDn + 0.49101*ODPDn + 2.1571*CICn + 0.35441*IACSn - 2.0085*PCIDn - 0.13073*PSn + 0.03479*NPn - 0.24313;
-
 		var cd_z = -Math.abs(0.70623)*OPPDn + 0.49539*ODMDn - 0.48514*ODPDn + 0.34646*CICn - 0.24829*IACSn + 0.057684*PCIDn - 0.66786*PSn - 0.089281*NPn - 1.3112;
-
 		var cd_h = -Math.abs(1.76)*OPPDn + 0.81515*ODMDn + 0.92478*ODPDn - 0.33256*CICn + 0.92847*IACSn + 0.07867*PCIDn - 0.54034*PSn - 0.4924*NPn - 0.072979;
-
 		var cd_th = 0.79609*OPPDn - 0.69284*ODMDn + 0.89944*ODPDn + 0.051006*CICn + 0.10291*IACSn + 1.0061*PCIDn + 0.64369*PSn - 0.027278*NPn + 2.8591;
 
 
 		//HIDDEN NEURONS
 		var HN1 = (Math.exp(cd_a) - Math.exp(-Math.abs(cd_a))) / (Math.exp(cd_a) + Math.exp(-Math.abs(cd_a)));
-
 		var HN2 = (Math.exp(cd_b) - Math.exp(-Math.abs(cd_b))) / (Math.exp(cd_b) + Math.exp(-Math.abs(cd_b)));
-
 		var HN3 = (Math.exp(cd_g) - Math.exp(-Math.abs(cd_g))) / (Math.exp(cd_g) + Math.exp(-Math.abs(cd_g)));
-
 		var HN4 = (Math.exp(cd_d) - Math.exp(-Math.abs(cd_d))) / (Math.exp(cd_d) + Math.exp(-Math.abs(cd_d)));
-
 		var HN5 = (Math.exp(cd_e) - Math.exp(-Math.abs(cd_e))) / (Math.exp(cd_e) + Math.exp(-Math.abs(cd_e)));
-
 		var HN6 = (Math.exp(cd_z) - Math.exp(-Math.abs(cd_z))) / (Math.exp(cd_z) + Math.exp(-Math.abs(cd_z)));
-
 		var HN7 = (Math.exp(cd_h) - Math.exp(-Math.abs(cd_h))) / (Math.exp(cd_h) + Math.exp(-Math.abs(cd_h)));
-
 		var HN8 = (Math.exp(cd_th) - Math.exp(-Math.abs(cd_th))) / (Math.exp(cd_th) + Math.exp(-Math.abs(cd_th)));
 
 
@@ -128,25 +62,7 @@ export default function ResultPage(props){
 	var CDOSn = (Math.exp(K) - Math.exp(-Math.abs(K))) / (Math.exp(K) + Math.exp(-Math.abs(K)));
 	// FINAL - CONSTRUCTION DELAY OWNER SIDE
 	var CDOS = 2*(CDOSn+1)+1;
-	//------------------------------------------END OF CALCULATIONS------------------------------------------//
-	return CDOS;
-	}
- 
-    return(
-			<Paper elevation={3} style={{height: 300}}>
-				<Grid className={classes.root} container direction="row" justify="center" alignItems="center" spacing={2}>
-					<Grid item xs={12}>
-						<Typography variant="h6" gutterBottom>{user} Side: {criteria}</Typography>
-					</Grid>
-					<Grid container direction="column" justify="flex-start" alignItems="center">
-						<Grid item xs={12}>
-						<Typography variant="h4" gutterBottom>{constdelayos}</Typography>
-						</Grid>
-						<Grid item xs={12}>
-							<Typography>is the most probable {criteria === "Construction Delay" ? "rate of delay for your project" : "degree of workmanship defects you will encounter in your project."}</Typography>
-					</Grid>
-					</Grid>
-        </Grid>
-			</Paper>
-    );
+
+//------------------------------------------END OF CALCULATIONS------------------------------------------//
+
 }
